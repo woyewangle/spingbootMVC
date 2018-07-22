2e26feb3c31ba5737b16ecb6af485b56a30ecc09
YuXiaoXi <1056596401@qq.com>
2018-07-23 00:23:25 +0800

    finish SpringBootMVC

---
+++ src/main/java/com/oocl/springbootmvc/Application.java
@@ -0,0 +1,17 @@
+package com.oocl.springbootmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Dylan Wei
+ * @date 2018-07-22 13:46
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
